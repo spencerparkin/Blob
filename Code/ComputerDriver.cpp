f3824baac3b8045ae3739adda4e87e5bbc6d9c37
Spencer T. Parkin <stparkin@mmm.com>
2016-12-21 16:25:26 -0700

    dummy computer driver for now

--- Code/ComputerDriver.cpp
+++ Code/ComputerDriver.cpp
@@ -1,6 +1,8 @@
 // ComputerDriver.cpp
 
 #include "ComputerDriver.h"
+#include "Blob.h"
+#include <Vector.h>
 
 ComputerDriver::ComputerDriver( void )
 {
@@ -12,6 +14,18 @@ ComputerDriver::ComputerDriver( void )
 
 /*virtual*/ void ComputerDriver::Drive( Blob* blob, const _3DMath::TimeKeeper& timeKeeper )
 {
+	// TODO: For now, just go forward all the time.  What I'd like to do is have them follow splines.
+
+	_3DMath::Vector unitAxleAxis;
+	blob->GetAxleAxis( unitAxleAxis );
+
+	double maxTorque = 0.0;
+	blob->maxTorque.GetValue( maxTorque );
+
+	_3DMath::ParticleSystem::TorqueForce* torqueForce = new _3DMath::ParticleSystem::TorqueForce( blob->GetParticleSystem() );
+	torqueForce->torque.SetScaled( unitAxleAxis, -maxTorque );
+	torqueForce->transient = true;
+	blob->GetParticleSystem()->forceList->push_back( torqueForce );
 }
 
 // ComputerDriver.cpp
